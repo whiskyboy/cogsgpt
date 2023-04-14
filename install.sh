676f34476a2dc3ca1b85e370cc881f1001fd7340
tiwe <weitian@microsoft.com>
2023-04-14 13:44:44 +0000

    add install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
+# Install dependencies
 sudo apt-get update
 sudo apt-get install build-essential libssl-dev ca-certificates libasound2 wget
+
+# Install python requirements
+pip install -r requirements.txt
+
+# activate env
+source .env
